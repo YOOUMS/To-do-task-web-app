766838741b1bfea48a33113fc3cca4a1ef401784
youms <yussef.msabeh@gmail.com>
2023-01-01 10:01:30 +0200

    feature: add local storage to save tasks

--- main.js
+++ main.js
@@ -3,13 +3,24 @@ let addTaskButton = document.getElementById('addTask');
 let taskNameInput = document.getElementById('taskName');
 let tasksContainer = document.getElementById('tasksContainer');
 
+
+if (window.localStorage['tasks'] != undefined) {
+    tasks = window.localStorage['tasks'].split(",");
+    fillTasks();
+}
+
+
+
 addTaskButton.addEventListener('click', function () {
 
     if (taskNameInput.value == '') {
         alert("Enter Task Name");
     } else {
+
+        console.log(typeof tasks);
         tasks.push(taskNameInput.value);
         taskNameInput.value = '';
+        window.localStorage['tasks'] = tasks;
         fillTasks();
     }
 });
@@ -20,25 +31,29 @@ addTaskButton.addEventListener('click', function () {
 function fillTasks() {
     tasksContainer.innerHTML = '';
     tasks.forEach(function (element) {
-
-        tasksContainer.innerHTML += `
+        if (element.length != 0) {
+            tasksContainer.innerHTML += `
     <div class="task-container">
         <span class="task-name">${element}</span>
         <button id="deleteTask" class="button delete-task">Delete</button>
     </div>
     
     `;
+        }
+
     });
 
 }
 
 document.addEventListener('click', function (e) {
-    console.log(e.target);
-
     if (e.target.className == "button delete-task") {
-        e.target.parentElement.remove();
         let name = e.target.parentElement.querySelector('.task-name').innerHTML;
-        delete tasks[tasks.indexOf(name)];
+        tasks = tasks.filter(function (element) {
+            return element != name;
+        })
+
+        window.localStorage['tasks'] = tasks;
+        fillTasks();
     }
 });
 
